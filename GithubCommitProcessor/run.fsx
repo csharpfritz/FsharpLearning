105940ee6750f3998e5a68e635dc1fcdcb57d0e2
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2018-07-06 13:10:51 -0400

    Updated with save to SQL database

--- GithubCommitProcessor/run.fsx
+++ GithubCommitProcessor/run.fsx
@@ -1,33 +1,77 @@
 #r "System.Net.Http"
 #r "Newtonsoft.Json"
+#r "System.Data"
+#r "EntityFramework.dll"
+#r "System.Configuration"
+
+// #load "data.fsx"
 
 open System.Net
 open System.Net.Http
 open Newtonsoft.Json
 open FSharp.Data
+open System
+open System.Configuration
+open System.ComponentModel.DataAnnotations
+open System.Data.Entity
 
-[<Literal>]
+
+
+[<CLIMutable>]
+type Metric = {
+
+    [<Key>]
+    Id: int
+    CommitId: string
+    Repository: string
+    DateStamp: DateTime
+    Name: string
+    NumFilesChanged: int
+}
+
+type GitHubData (name: string) = 
+    inherit DbContext (name: string)
+
+    [<DefaultValue>] val mutable metrics : DbSet<Metric>
+    member __.Metrics with get() = __.metrics and set v = __.metrics <- v
+
+
+[<Literal>] 
 let pushSample = __SOURCE_DIRECTORY__ + "/push.json"
 
 type PushWebHook = JsonProvider<pushSample> 
 
 let Run(req: HttpRequestMessage, log: TraceWriter) =
-    async {
+    async { 
         log.Info(sprintf 
-            "Handling a GitHub WebHook")
+            "Handling a GitHub WebHook") 
+
+        let dbContext = new GitHubData("fritzstreamdb")
 
         let! data = req.Content.ReadAsStringAsync() |> Async.AwaitTask
         let webhook = PushWebHook.Parse(data)
 
+        let commitId = webhook.After
+        let dateStamp = DateTime.UtcNow
         let repository = webhook.Repository.Name
 
-        // TODO: parse each commit's authors separately and give each of them appropriate credit
-        let author = webhook.Commits.[0].Author.Username
+        let filteredCommits = webhook.Commits |> Seq.filter(fun c -> c.Committer.Username <> "web-flow") 
 
+        for commit in filteredCommits do
+          
+          let newRecord:Metric = {
+            Id = 0;
+            CommitId = commitId;
+            Repository = repository;
+            DateStamp = dateStamp;
+            Name = commit.Author.Username;
+            NumFilesChanged = commit.Added.Length + commit.Removed.Length + commit.Modified.Length;
+          }
 
-        return req.CreateResponse(HttpStatusCode.OK, "Handled Push Webhook from " + repository + " from " + author)
+          dbContext.Metrics.Add newRecord |> ignore
+          
+        let! loaded = dbContext.SaveChangesAsync() |> Async.AwaitTask 
+        loaded |> printfn "Loaded %d records" 
+        return req.CreateResponse(HttpStatusCode.OK, "Handled Push Webhook from " + repository)
 
     } |> Async.RunSynchronously
-
-
-
